c67525fe08eaa5e4f64873ce128bf888f98ae975
Aaron202026 <66806616+Aaron202026@users.noreply.github.com>
2020-06-16 14:08:31 -0600

    nuevos

--- Jeffer_FERRETERIA/App_Start/BundleConfig.cs
+++ Jeffer_FERRETERIA/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Jeffer_FERRETERIA
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
