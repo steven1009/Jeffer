cd61999de95d9c8930734c556d776076ecf21001
Aaron202026 <66806616+Aaron202026@users.noreply.github.com>
2020-06-16 11:26:13 -0600

    eliminacion de codigo que no sirve

--- Jeffer_FERRETERIA/Content/js/Categoria.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="C:\Users\Samuel\Documents\Visual Studio 2015\Projects\FACTURA_JHONNY\WebFacturaMvc\Views/Producto/Create.cshtml" />
-$("#btnNuevo").click(function (eve) {
-    $("#modal-content").load("/Producto/Create");
-});
